0ef0e99fc433735e10590b643ec1bfc6bb451960
Matheus Souza dos santos <msdsantos96@gmail.com>
2024-08-19 07:58:07 -0300

    feat(url): criação do método de gerar url encurtada

--- prisma/migrations/20240817173813_create_users_and_url/migration.sql
+++ prisma/migrations/20240817173813_create_users_and_url/migration.sql
@@ -15,7 +15,7 @@ CREATE TABLE "urls" (
     "id" TEXT NOT NULL,
     "original_url" TEXT NOT NULL,
     "mini_url" TEXT NOT NULL,
-    "user_id" TEXT NOT NULL,
+    "user_id" TEXT ,
     "click_count" INTEGER NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3),
--- src/domain/url/enterprise/entities/url.ts
+++ src/domain/url/enterprise/entities/url.ts
@@ -2,6 +2,7 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 import { PreconditionFailedException } from '@nestjs/common'
 import { Entity } from 'src/core/entities/entity'
+import { randomBytes } from 'crypto'
 
 interface IUrlProps {
   originalUrl: string
@@ -64,6 +65,30 @@ export class Url extends Entity<IUrlProps> {
     this.props.deletedAt = new Date()
   }
 
+  createShortUrl() {
+    const upper = 'A'
+      .repeat(26)
+      .replace(/A/g, (_, i) => String.fromCharCode(65 + i)) // cria letras de A -Z
+
+    const lower = 'a'
+      .repeat(26)
+      .replace(/a/g, (_, i) => String.fromCharCode(97 + i)) // cria letras de a -
+
+    const numbers = '0'
+      .repeat(10)
+      .replace(/0/g, (_, i) => String.fromCharCode(48 + i))
+
+    const chars = `${upper}${lower}${numbers}` // gera um unico caracter
+
+    const length = Math.floor(Math.random() * 3) + 4 // gerar numero aléatorio entre 4 e 6
+
+    const bytes = randomBytes(length)
+
+    for (let i = 0; i < length; i++) {
+      this.miniUrl += chars[bytes[i] % chars.length]
+    }
+  }
+
   private touch() {
     this.props.updatedAt = new Date()
   }
@@ -75,6 +100,7 @@ export class Url extends Entity<IUrlProps> {
     const url = new Url(
       {
         ...props,
+        clickCount: 0,
         createdAt: props.createdAt ?? new Date(),
         deletedAt: props.deletedAt ?? new Date(),
       },
